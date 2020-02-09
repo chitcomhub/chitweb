50558a47f2b3fc261a8bea178a10e95c5416223e
Urus-Martan-95 <59649003+Urus-Martan-95@users.noreply.github.com>
2020-02-09 15:44:17 +0300

    Add burger menu (#7)
    * Add burger menu
    
    * add fixed mobile layout
    
    * add fixed breakpoints to mobile and tablet
    
    * add links to burger menu

--- chitweb/static/js/project.js
+++ chitweb/static/js/project.js
@@ -1,1 +1,6 @@
 /* Project specific Javascript goes here. */
+$(document).ready(function(){
+  $('.header_burger').click(function(event){
+    $('.header_burger,.header__menu').toggleClass('active');
+  });
+});
