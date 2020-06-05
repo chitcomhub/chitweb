606e38e0f3d5da86996158f0d2820509f86b0156
Askhab Musaev <44657540+42musaev@users.noreply.github.com>
2020-06-05 23:48:56 +0300

    Created models specialization, specialist (#9)
    * Created models specialization, specialist
    
    * Created models specialization, specialist
    
    * Revert "Created models specialization, specialist"
    
    This reverts commit 6765a08adb22deb96c8ce387f46f3992056112dd.
    
    * Ignore idea files
    
    * Add specialists model migration
    
    * Revert "Add specialists model migration"
    
    This reverts commit 4e31a615f3d51d6e5724d85e7ec15668bd5b2bb4.
    
    * update specialist and specialization models
    
    Co-authored-by: nmago <e-five@mail.ru>
    Co-authored-by: ai-rage <azamat.murdalov@mail.ru>

--- chitweb/specialists/admin.py
+++ chitweb/specialists/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-# TODO: add models in specailists app
-# and uncomment this code
-
-# from .models import Specialist
+from .models import Specialist
+from .models import Specialization
 
 
-# admin.site.register(Specialist)
+admin.site.register(Specialist)
+admin.site.register(Specialization)
--- chitweb/specialists/models.py
+++ chitweb/specialists/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Specialization(models.Model):
+    title = models.CharField("Специализация", max_length=120, default="",)
+
+    def __str__(self):
+        return self.title
+
+
+class Specialist(models.Model):
+    first_name = models.CharField("Имя", max_length=120, default="",)
+    last_name = models.CharField("Фамилия", max_length=120, default="",)
+    short_bio = models.CharField("Описание", max_length=200, default="",)
+    long_bio = models.TextField("Полное описание", blank=True, default="",)
+    telegram = models.CharField("Telegram", max_length=120, default="",)
+    github = models.CharField("Github", max_length=120, default="", blank=True,)
+    photo = models.ImageField(
+        default="default-profile.png", upload_to="profile-pics", verbose_name="Фото",
+    )
+    specializations = models.ManyToManyField(
+        "Specialization", verbose_name="Специализации"
+    )
+
+    def __str__(self):
+        return self.first_name
